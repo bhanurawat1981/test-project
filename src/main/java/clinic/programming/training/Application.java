b604c5b95a7ef0a8c8916ccd9c5850b20da88be5
Bhanu Pratap Singh Rawat <bharawat@publicisgroupe.net>
2019-11-25 10:48:25 +0530

    added test class and dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -4,26 +4,57 @@ import java.util.List;
 import java.util.ArrayList;
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * @author bharawat
+ *
+ */
 public class Application {
-    
-    public Application() {
-        System.out.println ("Inside Application");
-    }
 
-    // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
-	List<String> stringList = new ArrayList<>();
-	stringList.add("I am here.");
-	stringList.add("Where are you?");
-	stringList.add(" ");
-	for(String str : stringList){
-	   if(StringUtils.isNotBlank(str)){
-		System.out.println("Message : "+str);
-	   }else{
-		System.out.println("Blank string encountered.");
-	   }
-	}	
-    }
+	public Application() {
+		System.out.println("Inside Application");
+	}
+
+	// method main(): ALWAYS the APPLICATION entry point
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Starting Application");
+		Application application = new Application();
+		// creating message list.
+		List<String> stringList = new ArrayList<>();
+		// adding messages to list.
+		application.printMessage("I am here.", stringList);
+		application.printMessage("Where are you?", stringList);
+		application.printMessage(" ", stringList);
+		// printing messages in list.
+		application.printMessageList(stringList);
+		System.out.println("Closing Application");
+	}
+
+	/**
+	 * @param message
+	 * @param stringList
+	 */
+	public void printMessage(String message, List<String> stringList) {
+		stringList.add(message);
+		if (StringUtils.isNotBlank(message)) {
+			System.out.println("Message : " + message);
+		} else {
+			System.out.println("Blank string encountered.");
+		}
+	}
+
+	/**
+	 * @param stringList
+	 */
+	public void printMessageList(List<String> stringList) {
+		for (String message : stringList) {
+			if (StringUtils.isNotBlank(message)) {
+				System.out.println("Message : " + message);
+			} else {
+				System.out.println("Blank string encountered.");
+			}
+		}
+	}
 }
